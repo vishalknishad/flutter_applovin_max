80c690e91a1ec9c23fc293b1d2aee57700be722f
elkhalifte <39893327+elkhalifte@users.noreply.github.com>
2021-10-22 16:35:34 +0200

    Update banner.dart

--- lib/banner.dart
+++ lib/banner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_applovin_max/flutter_applovin_max.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:expandable_page_view/expandable_page_view.dart';
 import 'dart:io';
 
 enum BannerAdSize {
@@ -29,12 +30,30 @@ class BannerMaxView extends StatelessWidget {
   };
   final BannerAdSize size;
   final String adUnitId;
-
+  late List<Widget> screens;
+  late AndroidView androidView;
+  String banner_value = "";
+  bool visible_banner = true;
+  PageController controller = new PageController(initialPage: 0);
   BannerMaxView(this.listener, this.size, this.adUnitId, {Key? key})
       : super(key: key);
 
+  initState() {
+    screens = [
+      Container(
+          width: sizesNum[size]?.width,
+          height: sizesNum[size]?.height,
+          child: Platform.isAndroid ? androidView : null),
+      Container(
+        // height: 30,
+        color: Colors.transparent,
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
+    initState();
     final AndroidView androidView = AndroidView(
         viewType: '/Banner',
         key: UniqueKey(),
@@ -43,11 +62,31 @@ class BannerMaxView extends StatelessWidget {
         onPlatformViewCreated: (int i) {
           const MethodChannel channel = MethodChannel('flutter_applovin_max');
           channel.setMethodCallHandler((MethodCall call) async =>
-              FlutterApplovinMax.handleMethod(call, listener));
+              FlutterApplovinMax.handleMethod(call,
+                  (AppLovinAdListener? event) {
+                if (event == AppLovinAdListener.adLoaded) {
+                  banner_value = "load";
+                  controller.jumpToPage(0);
+                  Timer(Duration(seconds: 2), () {
+                    print('timer ended');
+                    print(banner_value);
+                    if (banner_value == "load") {
+                      controller.jumpToPage(1);
+                    }
+                  });
+                } else if (event == AppLovinAdListener.adDisplayed) {
+                  banner_value = "display";
+                  controller.jumpToPage(0);
+                } else {
+                  banner_value = "other";
+                  controller.jumpToPage(1);
+                }
+              }));
         });
-    return Container(
-        width: sizesNum[size]?.width,
-        height: sizesNum[size]?.height,
-        child: Platform.isAndroid ? androidView : null);
+    return ExpandablePageView(
+      physics: NeverScrollableScrollPhysics(),
+      controller: controller,
+      children: screens,
+    );
   }
 }
